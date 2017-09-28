5511fe27ea9946dd0f370f1226402134247d6f91
Matt <mguest55@yahoo.com>
2017-09-28 13:30:29 -0700

    Worked on Login screen UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by The Guest Family on 9/28/17.
+//  Copyright © 2017 AlphaApplications. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by The Guest Family on 9/28/17.
+//  Copyright © 2017 AlphaApplications. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
