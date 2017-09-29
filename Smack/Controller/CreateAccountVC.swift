307aa7e3f1fbb5e67e0b9a7f304cb0656f13e97f
Matt <mguest55@yahoo.com>
2017-09-28 19:54:30 -0700

    Worked a bit on user authentication

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -16,6 +16,9 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
+    // Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,18 +30,30 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccountPressed(_ sender: Any) {
+        guard let name = usernameTxt.text , usernameTxt.text != "" else { return }
         guard let email = emailTxt.text , emailTxt.text != "" else { return }
         guard let pass = passTxt.text , passTxt.text != "" else { return }
         
+        // Process as described in API, first registerUser, second loginUser, finally create User
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
-            
             if success {
-                print("Registered The User!")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                // Unwind might be a little slow, just an fyi
+                                print(UserDataService.instance.name,
+                                      UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
+                    }
+                })
             }
         }
     }
     
-    @IBAction func pickAvaterPressed(_ sender: Any) {
+    @IBAction func pickAvatarPressed(_ sender: Any) {
         
     }
     
---
+++ Smack/Services/UserDataService.swift
@@ -0,0 +1,34 @@
+//
+//  UserDataService.swift
+//  Smack
+//
+//  Created by The Guest Family on 9/28/17.
+//  Copyright © 2017 AlphaApplications. All rights reserved.
+//
+
+import Foundation
+
+class UserDataService {
+    
+    static let instance = UserDataService()
+    
+    // Can be read publicly, but set privately only this file can modify var id
+    public private(set) var id = ""
+    public private(set) var avatarColor = ""
+    public private(set) var avatarName = ""
+    public private(set) var email = ""
+    public private(set) var name = ""
+    
+    func setUserData(id: String, color: String, avatarName: String, email: String, name: String) {
+        self.id = id
+        self.avatarColor = color
+        self.avatarName = avatarName
+        self.email = email
+        self.name = name
+    }
+    
+    func setAvaterName(avaterName: String) {
+        self.avatarName = avaterName
+    }
+    
+}
