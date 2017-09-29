3df9d20835eb685939ce9cc89c35875956af8315
Matt <mguest55@yahoo.com>
2017-09-28 17:40:25 -0700

    Started work on AuthService

---
+++ Smack/Services/AuthService.swift
@@ -0,0 +1,80 @@
+//
+//  AuthServices.swift
+//  Smack
+//
+//  Created by The Guest Family on 9/28/17.
+//  Copyright © 2017 AlphaApplications. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    // Create singleton here, accessible globally
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    // Variables (computed properties) can be read or written to from anywhere in the app
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, pasword: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        
+        // Key value pair can be found in Postman headers, below are JSON objects
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            
+            if response.result.error == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+}
+
+
+
+
+
