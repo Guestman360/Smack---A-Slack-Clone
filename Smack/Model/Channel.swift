2c6fe3809763f3c24bb484575781880fb3e21d28
Matt <mguest55@yahoo.com>
2017-09-29 20:48:38 -0700

    Added Channel model

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,17 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by The Guest Family on 9/29/17.
+//  Copyright © 2017 AlphaApplications. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+    
+}
